1accf8d474db99a56176fc578774c7e1fc73c2d8
RFInfo <103699779+RFInfo@users.noreply.github.com>
2024-11-04 22:57:24 +0200

    updates

--- src/edu/info/util/ImageUtil.java
+++ src/edu/info/util/ImageUtil.java
@@ -40,8 +40,13 @@ public class ImageUtil {
         frame.pack();
         frame.setVisible(true);
     }
+
+    public static void displayImage(BufferedImage img) {
+        displayImage(img, "");
+    }
     
     public static BufferedImage generateRandom(int width, int height){
+//        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
         Random rand = new Random();
 
@@ -56,25 +61,29 @@ public class ImageUtil {
     }
 
     public static BufferedImage extractBand(BufferedImage inImg, char band){
-        BufferedImage outImg = new BufferedImage(inImg.getWidth(), inImg.getHeight(),BufferedImage.TYPE_BYTE_GRAY);
+        BufferedImage outImg = new BufferedImage(inImg.getWidth(),inImg.getHeight(),BufferedImage.TYPE_BYTE_GRAY);
 
         for (int y = 0; y < inImg.getHeight(); y++)
             for (int x = 0; x < inImg.getWidth(); x++) {
                 int pixel = inImg.getRGB(x,y);
 
-                int alpha = (pixel & 0xff000000) >> 24;
-                int red   = (pixel & 0x00ff0000) >> 16;
-                int green = (pixel & 0x0000ff00) >> 8;
-                int blue  = (pixel & 0x000000ff);
+                int alpha = (pixel & 0xff000000) >> 24; // (pixel >> 24) & 0xff
+                int red =   (pixel & 0x00ff0000) >> 16; // (pixel >> 16) & 0xff
+                int green = (pixel & 0x0000ff00) >> 8;  // (pixel >> 8) & 0xff
+                int blue =  (pixel & 0x000000ff);       // (pixel) & 0xff
 
-                if(y == 0)
-                    System.out.print(alpha + " " + red + " " + green + " " + blue + " / ");
+                // how to recompose
+                //pixel = 0x00000000 | (alpha << 24) | (red << 16) | (green << 8) | blue;
+
+                // print first row
+//                if(y == 0)
+//                    System.out.println(alpha + " " + red + " " + green + " " + blue);
 
                 switch (band){
-                    case 'R' -> outImg.getRaster().setSample(x,y,0, red);
-                    case 'G' -> outImg.getRaster().setSample(x,y,0, green);
-                    case 'B' -> outImg.getRaster().setSample(x,y,0, blue);
-                    case 'A' -> outImg.getRaster().setSample(x,y,0, alpha);
+                    case 'A' -> outImg.getRaster().setSample(x,y,0,alpha);
+                    case 'R' -> outImg.getRaster().setSample(x,y,0,red);
+                    case 'G' -> outImg.getRaster().setSample(x,y,0,green);
+                    case 'B' -> outImg.getRaster().setSample(x,y,0,blue);
                 }
             }
         return outImg;
